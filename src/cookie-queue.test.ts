4924b09f9782eea186833184c5c4caf3c5f6214b
mareklabuz <mrk.labuz@gmail.com>
2022-12-15 21:49:45 +0100

    Implement and test the described scenario

--- src/cookie-queue.test.ts
+++ src/cookie-queue.test.ts
@@ -1,7 +1,123 @@
-import { describe, expect, test } from "@jest/globals";
+import { describe, expect, it, jest } from "@jest/globals";
+import { CookieQueue } from ".";
+import { Cookie } from "./cookie";
+import { CookieType } from "./cookie-type.enum";
+
+const getGoodCookie = (() => {
+    const possibleTypes = [
+        CookieType.CHRISTMAS_TREE,
+        CookieType.SANTA_CLAUS_HAT,
+    ];
+    let index = 1;
+    return (): Cookie => {
+        const label = `label-${index}`;
+        const type = possibleTypes[index % possibleTypes.length];
+        index += 1;
+        return new Cookie(label, type);
+    };
+})();
 
 describe("Cookie queue", () => {
-    test("test", () => {
-        // TODO
+    it("should not report cookies with errors", () => {
+        const cookieQueue = new CookieQueue();
+        const goodCookies: Cookie[] = [];
+
+        for (let i = 0; i < 10; i += 1) {
+            const goodCookie = getGoodCookie();
+            goodCookies.push(goodCookie);
+            cookieQueue.add(goodCookie);
+        }
+
+        cookieQueue.add(new Cookie("bad-cookie-1", CookieType.ERROR));
+        cookieQueue.add(new Cookie("bad-cookie-2", CookieType.ERROR));
+        cookieQueue.add(new Cookie("bad-cookie-3", CookieType.ERROR));
+
+        for (let i = 0; i < 10; i += 1) {
+            const goodCookie = getGoodCookie();
+            goodCookies.push(goodCookie);
+            cookieQueue.add(goodCookie);
+        }
+
+        goodCookies.reverse();
+
+        const reportedCookies = cookieQueue.getReport().getCookies();
+        expect(reportedCookies).toHaveLength(20);
+        expect(reportedCookies).toEqual(goodCookies);
+    });
+
+    it("should only report last 100 cookies", () => {
+        const cookieQueue = new CookieQueue();
+        const cookies: Cookie[] = [];
+
+        for (let i = 0; i < 150; i += 1) {
+            const goodCookie = getGoodCookie();
+            cookies.push(goodCookie);
+            cookieQueue.add(goodCookie);
+        }
+
+        cookies.reverse();
+
+        const reportedCookies = cookieQueue.getReport().getCookies();
+        expect(reportedCookies).toHaveLength(100);
+        expect(reportedCookies).toEqual(cookies.slice(0, 100));
+    });
+
+    it("should not report expired cookies", () => {
+        jest.useFakeTimers();
+
+        let fakeNow = Date.now();
+        const threeMinutesInMilliseconds = 1000 * 60 * 3;
+        const cookieQueue = new CookieQueue();
+        const earlyCookies: Cookie[] = [];
+        const lateCookies: Cookie[] = [];
+
+        for (let i = 0; i < 50; i += 1) {
+            const earlyCookie = getGoodCookie();
+            earlyCookies.push(earlyCookie);
+            cookieQueue.add(earlyCookie);
+        }
+
+        // Advance time by three minutes to add late cookies
+        fakeNow += threeMinutesInMilliseconds;
+        jest.setSystemTime(fakeNow);
+
+        for (let i = 0; i < 50; i += 1) {
+            const lateCookie = getGoodCookie();
+            lateCookies.push(lateCookie);
+            cookieQueue.add(lateCookie);
+        }
+
+        earlyCookies.reverse();
+        lateCookies.reverse();
+
+        let reportedCookies = cookieQueue.getReport().getCookies();
+        expect(reportedCookies).toHaveLength(100);
+        expect(reportedCookies).toEqual([...lateCookies, ...earlyCookies]);
+
+        // Advance time by another three minutes to expire early cookies
+        fakeNow += threeMinutesInMilliseconds;
+        jest.setSystemTime(fakeNow);
+
+        reportedCookies = cookieQueue.getReport().getCookies();
+        expect(reportedCookies).toHaveLength(50);
+        expect(reportedCookies).toEqual(lateCookies);
+    });
+
+    it("should return empty report when all cookies expire", () => {
+        jest.useFakeTimers();
+
+        const now = Date.now();
+        const cookieQueue = new CookieQueue();
+
+        for (let i = 0; i < 50; i += 1) {
+            cookieQueue.add(getGoodCookie());
+        }
+
+        // Advance time by five minutes to expire all cookies
+        jest.setSystemTime(now + 1000 * 60 * 5);
+
+        const reportedCookies = cookieQueue.getReport().getCookies();
+        expect(reportedCookies).toHaveLength(0);
+        expect(reportedCookies).toEqual([]);
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,102 @@
-// YOUR CODE GOES HERE
-console.log("Merry Christmas!");
+import { Cookie } from "./cookie";
+import { ICookieQueue } from "./cookie-queue.interface";
+import { CookieType } from "./cookie-type.enum";
+import { IReport } from "./report.interface";
+
+class Box {
+    nextBox: Box | null = null;
+    prevBox: Box | null = null;
+
+    readonly expiryDate: Date;
+
+    constructor(public readonly cookie: Cookie) {
+        const now = Date.now();
+        const fiveMinutesInMilliseconds = 1000 * 60 * 5;
+        this.expiryDate = new Date(now + fiveMinutesInMilliseconds);
+    }
+
+    isExpired(): boolean {
+        return Date.now() >= this.expiryDate.getTime();
+    }
+}
+
+// Given implementation optimizes adding cookies by using a doubly linked list
+export class CookieQueue implements ICookieQueue {
+    private firstBox: Box | null = null;
+    private lastBox: Box | null = null;
+    private boxCount = 0;
+
+    private readonly NO_COOKIES_IN_REPORT = 100;
+
+    add(cookie: Cookie): void {
+        if (cookie.getCookieType() === CookieType.ERROR) {
+            return;
+        }
+
+        const box = new Box(cookie);
+
+        // Check if the list is empty
+        if (!this.firstBox || !this.lastBox) {
+            this.firstBox = box;
+            this.lastBox = box;
+        } else {
+            // Add box to the front of the list
+            box.nextBox = this.firstBox;
+            this.firstBox.prevBox = box;
+            this.firstBox = box;
+        }
+
+        this.boxCount += 1;
+        this.removeUnnecessaryBoxes();
+    }
+
+    getReport(): IReport {
+        this.removeUnnecessaryBoxes();
+
+        const cookies: Cookie[] = [];
+        let box = this.firstBox;
+
+        for (let i = 0; i < this.NO_COOKIES_IN_REPORT; i += 1) {
+            if (!box) {
+                break;
+            }
+            cookies.push(box.cookie);
+            box = box.nextBox;
+        }
+
+        return {
+            getCookies: () => cookies,
+        };
+    }
+
+    private removeUnnecessaryBoxes(): void {
+        while (this.boxCount > this.NO_COOKIES_IN_REPORT) {
+            this.removeBoxFromBack();
+        }
+
+        while (this.lastBox?.isExpired()) {
+            this.removeBoxFromBack();
+        }
+    }
+
+    private removeBoxFromBack(): void {
+        if (!this.lastBox) {
+            return;
+        }
+
+        // Check if there is just one box in the list
+        if (!this.lastBox.prevBox) {
+            this.firstBox = null;
+            this.lastBox = null;
+            this.boxCount = 0;
+            return;
+        }
+
+        // Remove box from the back of the list
+        const newLastBox = this.lastBox.prevBox;
+        this.lastBox.prevBox.nextBox = null;
+        this.lastBox.prevBox = null;
+        this.lastBox = newLastBox;
+        this.boxCount -= 1;
+    }
+}
